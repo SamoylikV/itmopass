585adfeb9450701f56bb809844a3d4467bb82538
ToxicVasiliy <vasya.samoylik@gmail.com>
2023-06-19 21:36:21 +0300

    Initial Commit

---
+++ ITMO.Pass Watch App/ITMO_PassApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITMO_PassApp.swift
+//  ITMO.Pass Watch App
+//
+//  Created by Samoylik on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ITMO_Pass_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ITMO.Pass/ITMO_PassApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITMO_PassApp.swift
+//  ITMO.Pass
+//
+//  Created by Samoylik on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ITMO_PassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
